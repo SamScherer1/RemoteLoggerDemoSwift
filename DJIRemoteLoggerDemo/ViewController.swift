6a6bba1d71c45276f97c5b22f4cafe1f5dad661d
SamScherer1 <samscherer33@gmail.com>
2021-05-20 18:38:17 -0400

    fix registration error, appkey

--- DJIRemoteLoggerDemo/ViewController.swift
+++ DJIRemoteLoggerDemo/ViewController.swift
@@ -24,20 +24,22 @@ class ViewController: UIViewController, DJISDKManagerDelegate {
         self.present(alertController, animated: true, completion: nil)
     }
     
-//
     //MARK: - DJISDKManager Delegate Method
     func appRegisteredWithError(_ error: Error?) {
         var message = "Register App Successed!"
-        if let _ = error {
-            message = "Register App Failed! Please enter your App Key and check the network."
+        if let error = error {
+            message = "Register App Failed! Please enter your App Key and check the network. Error: \(error.localizedDescription)"
         } else {
-            DJISDKManager.enableRemoteLogging(withDeviceID: "Enter Device ID Here", logServerURLString: "Enter URL Here")
+            // DeviceID can be whatever you'd like
+            // URLString is provided in green when starting the server. It should begin with http:// and end with a port number
+            DJISDKManager.enableRemoteLogging(withDeviceID: "DeviceID", logServerURLString: "http://192.168.128.181:4567")
         }
         self.showAlertViewWith(title: "Register App", message: message)
     }
 
     func productConnected(_ product: DJIBaseProduct?) {
-        //If this demo is used in China, it's required to login to your DJI account to activate the application. Also you need to use DJI Go app to bind the aircraft to your DJI account. For more details, please check this demo's tutorial.
+        //If this demo is used in China, it's required to login to your DJI account to activate the application.
+        //Also you need to use DJI Go app to bind the aircraft to your DJI account. For more details, please check this demo's tutorial.
         DJISDKManager.userAccountManager().logIntoDJIUserAccount(withAuthorizationRequired: false) { (state:DJIUserAccountState, error:Error?) in
             if let error = error {
                 print("Login failed: \(error.localizedDescription)")
@@ -47,8 +49,7 @@ class ViewController: UIViewController, DJISDKManagerDelegate {
 
     //#MARK: - IBAction Method
     @IBAction func logSDKVersionButtonAction(_ sender: Any) {
-        //        DJILogDebug(@"SDK Version: %@", [DJISDKManager SDKVersion]);
-        
+        DJIRemoteLogger.log(with: .debug, file: #file, function: #function, line: #line, string: DJISDKManager.sdkVersion())
     }
 
     func didUpdateDatabaseDownloadProgress(_ progress: Progress) {
